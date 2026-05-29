50111954860d5905847781577174fac3e1b2106f
pachez <pascheznation@gmail.com>
2026-05-30 00:28:19 +0100

    updated the project and added navbar responsiveness

--- hotel-project/src/components/Navbar/Hamburger.jsx
+++ hotel-project/src/components/Navbar/Hamburger.jsx
@@ -1,10 +1,10 @@
 
 // import React from 'react'
 
-function Hamburger({images, toggleMenu}) {
+function Hamburger({images, toggleMenu, isOpen}) {
   return (
-    <div className="w-10 z-50 hover:bg-blue-300 rounded-full active:bg-gray-100 p-2 cursor-pointer" onClick={toggleMenu} >
-        <img src={images.menu} alt="" className=" block w-full md:hidden " />
+    <div className="sm:hidden w-10 z-50 hover:bg-blue-300 rounded-full active:bg-gray-100 p-2 cursor-pointer" onClick={toggleMenu} >
+       {isOpen ? <div>x</div>:  <img src={images.menu} alt="" className=" block w-full md:hidden text-white  shadow-4xl" />}
     </div>
   )
 }
--- hotel-project/src/components/Navbar/Navbar.jsx
+++ hotel-project/src/components/Navbar/Navbar.jsx
@@ -6,23 +6,32 @@ import NavLinks from "./NavLinks"
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+  // const [bgScreenIsOpen, setBgScreenIsOpen] = useState(false)
 
-  const toggleMenu = () =>{
-    setIsOpen(!isOpen);
+
+  const toggleMenu = () => {
+    setIsOpen(prev => !prev);
   }
 
   return (
     <div className="flex w-full h-8 fixed bg-white shadow-sm top-0 items-center px-8 sm:px-20 py-2  z-50 h-15">
-    <div className=" flex justify-between w-full">
-      
-      
+      <div className=" flex justify-between w-full">
+
+
         <p className="text-xl"><span className="text-blue-600">Lanka</span>Stay.</p>
-        {isOpen &&  <NavLinks />  }
-       
-       
-         
-    </div>
-    <Hamburger images={images} toggleMenu ={toggleMenu} />
+
+        <div className="hidden sm:flex">
+          <NavLinks />
+        </div>
+
+        {isOpen && (
+        <div className=" top-0 left-0 flex w-full flex-col bg-white shadow-md lg:hidden">
+          <NavLinks />
+        </div>)}
+
+
+      </div>
+      <Hamburger images={images} toggleMenu={toggleMenu} isOpen={isOpen} />
     </div>
   )
 }
